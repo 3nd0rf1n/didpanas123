4336a7b4c679b7617e43575c035a66d30d18a619
3nd0rf1n <saylen2006@gmail.com>
2025-07-21 20:35:19 +0300

    132123132
    123

--- mybot.py
+++ mybot.py
@@ -787,4 +787,14 @@ async def main():
     await application.run_polling()
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    import asyncio
+
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = None
+
+    if loop and loop.is_running():
+        asyncio.create_task(main())  # Якщо цикл вже працює
+    else:
+        asyncio.run(main())  # Якщо ще не запущений
